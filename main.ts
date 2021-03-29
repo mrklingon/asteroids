8d3a33649c599cca89a18b483bbd278e633aabb6
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-03-29 11:38:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    fuel = 4
+})
+input.onButtonPressed(Button.B, function () {
+    DujwIj.turn(Direction.Right, 45)
+})
+let fuel = 0
+let DujwIj: game.LedSprite = null
+DujwIj = game.createSprite(2, 2)
+fuel = 0
+basic.forever(function () {
+    DujwIj.move(fuel)
+    fuel += -0.5
+    if (fuel < 0) {
+        fuel = 0
+    }
+    DujwIj.ifOnEdgeBounce()
+    basic.pause(200)
+})
